684570026b045737993337ad562127dac9faa6ad
Tristan Brotherton <tristan@myvr.com>
2020-01-22 12:16:13 -0800

    Add support for sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var reload = browserSync.reload;
@@ -20,12 +21,19 @@ gulp.task('serve', ['sass'], function() {
 });
 
 // Compile sass into CSS
-gulp.task('sass', function() {
-    return gulp
-        .src(src.scss)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(src.css))
-        .pipe(reload({ stream: true }));
+// gulp.task('sass', function() {
+//     return gulp
+//         .src(src.scss)
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(gulp.dest(src.css))
+//         .pipe(reload({ stream: true }));
+// });
+gulp.task('sass', function () {
+ return gulp.src('./sass/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('./css'));
 });
 
 gulp.task('default', ['serve']);
