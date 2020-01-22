ef544391d2786c5ab556174754efba4198ad34d1
Tristan Brotherton <tristan@myvr.com>
2020-01-22 12:06:09 -0800

    Update to use ruby.sass with sourcemapping

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var filter = require('gulp-filter');
+var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var reload = browserSync.reload;
 
 var src = {
@@ -9,9 +11,27 @@ var src = {
     html: 'app/*.html'
 };
 
-// Static Server + watching scss/html files
+/**
+ * Kick off the sass stream with source maps + error handling
+ */
+function sassStream() {
+    return sass('app/scss', { sourcemap: true })
+        .on('error', function(err) {
+            console.error('Error!', err.message);
+        })
+        .pipe(
+            sourcemaps.write('./', {
+                includeContent: false,
+                sourceRoot: '/app/scss'
+            })
+        );
+}
+
+/**
+ * Start the Browsersync Static Server + Watch files
+ */
 gulp.task('serve', ['sass'], function() {
-    browserSync.init({
+    browserSync({
         server: './app'
     });
 
@@ -19,13 +39,14 @@ gulp.task('serve', ['sass'], function() {
     gulp.watch(src.html).on('change', reload);
 });
 
-// Compile sass into CSS
+/**
+ * Compile sass, filter the results, inject CSS into all browsers
+ */
 gulp.task('sass', function() {
-    return gulp
-        .src(src.scss)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(src.css))
-        .pipe(reload({ stream: true }));
+    return sassStream().pipe(gulp.dest(src.css)).pipe(filter('**/*.css')).pipe(reload({ stream: true }));
 });
 
+/**
+ * Default task
+ */
 gulp.task('default', ['serve']);
